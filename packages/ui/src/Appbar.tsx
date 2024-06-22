85c7b424d12b37e8e9906ab2b5cf1b74f26aa904
Meet Jain <meetjaiin@gmail.com>
2024-06-22 10:55:21 +0530

    fixed bugs

--- packages/ui/src/Appbar.tsx
+++ packages/ui/src/Appbar.tsx
@@ -4,7 +4,7 @@ interface AppbarProps {
     user?: {
         name?: string | null;
     },
-    // TODO: can u figure out what the type should be here?
+   
     onSignin: any,
     onSignout: any
 }
@@ -13,13 +13,16 @@ export const Appbar = ({
     user,
     onSignin,
     onSignout
-}: AppbarProps) => {
+}: 
+
+    AppbarProps) => {
+
     return <div className="flex justify-between border-b px-4">
         <div className="text-lg flex flex-col justify-center">
-            PayTM
+            PayNext Wallet
         </div>
         <div className="flex flex-col justify-center pt-2">
             <Button onClick={user ? onSignout : onSignin}>{user ? "Logout" : "Login"}</Button>
         </div>
-    </div>
+    </div> 
 }
